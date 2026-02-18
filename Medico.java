de0aa8041146beb4277a998f2f727f39e5d709ab
Diegoerfuego <diegoraffaelli08@gmail.com>
2026-02-18 13:18:34 +0100

    Medico.java

--- Medico.java
+++ Medico.java
@@ -1,47 +1,74 @@
-import java.util.ArrayList;
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Medico extends Persona {
 
-
     private String specializzazione;
-
-    // Lista che contiene tutte le visite (prenotazioni) del medico
-    private ArrayList<Visita> prenotazioni;
-
+    private Queue<Visita> prenotazioni;
 
     public Medico(String nome, String cognome, String specializzazione) {
         super(nome, cognome);
         this.specializzazione = specializzazione;
-        this.prenotazioni = new ArrayList<Visita>();
+        this.prenotazioni = new LinkedList<>();
     }
-
 
     public String getSpecializzazione() {
         return specializzazione;
     }
 
-    // Metodo che restituisce la lista delle prenotazioni
-    public ArrayList<Visita> getPrenotazioni() {
-        return prenotazioni;
-    }
-
-    ì
     public void aggiungiVisita(Visita v) {
-        prenotazioni.add(v); // Inserisce la visita nella lista
+        prenotazioni.add(v); // inserimento in coda
     }
 
+    // Il medico effettua la prossima visita (FIFO)
+    public void effettuaProssimaVisita() {
+
+        if (!prenotazioni.isEmpty()) {
+
+            Visita v = prenotazioni.poll(); // rimuove il primo
+            v.setEffettuata();
+
+            System.out.println("Visita effettuata: " + v);
+
+        } else {
+            System.out.println("Nessuna visita in coda.");
+        }
+    }
 
     public void visualizzaPrenotazioni() {
 
-        // Stampa il nome del medico e la sua specializzazione
-        System.out.println("Prenotazioni del Dottor " + getCognome() + "(" + specializzazione + ")");
+        System.out.println("\nPrenotazioni del Dottor "
+                + getCognome() + " (" + specializzazione + ")");
 
-        // Ciclo che scorre tutte le visite nella lista
-        for (int i = 0; i < prenotazioni.size(); i++) {
+        int size = prenotazioni.size();
 
-            // Stampa la visita alla posizione i
-            System.out.println(prenotazioni.get(i));
+        for (int i = 0; i < size; i++) {
+
+            Visita v = prenotazioni.poll(); // tolgo
+            System.out.println(v);          // stampo
+            prenotazioni.add(v);           // reinserisco
         }
+    }
+
+    public boolean rimuoviVisita(String nomePaziente) {
+
+        boolean rimossa = false;
+
+        for (int i = 0; i < prenotazioni.size; i++) {
+
+            Visita v = prenotazioni.poll();
+
+            if (v.getNomePaziente().equals(nomePaziente)
+                    && v.getStato().equals("prenotata")
+                    && !rimossa) {
+
+                rimossa = true; // NON la reinserisco (quindi eliminata)
+
+            } else {
+                prenotazioni.add(v); // la rimetto in coda
+            }
+        }
+
+        return rimossa;
     }
 }
