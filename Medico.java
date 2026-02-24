054708462a1f072c4dc4b410a0e7724f0e952d96
Diegoerfuego <diegoraffaelli08@gmail.com>
2026-02-24 13:54:48 +0100

    Medico.java

--- Medico.java
+++ Medico.java
@@ -1,7 +1,9 @@
+package it.edu.cannizzaro.quartab25.poliambulatorio;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Medico extends Persona {
+
     private String specializzazione;
     private Queue<Visita> prenotazioni;
 
@@ -11,45 +13,59 @@ public class Medico extends Persona {
         this.prenotazioni = new LinkedList<>();
     }
 
+    public String getSpecializzazione() {
+        return specializzazione;
+    }
+
     public void aggiungiVisita(Visita v) {
-        prenotazioni.add(v);
+        prenotazioni.add(v); // inserimento in coda
+    }
+
+    // Il medico effettua la prossima visita
+    public void effettuaProssimaVisita() {
+
+        if (!prenotazioni.isEmpty()) {
+
+            Visita v = prenotazioni.poll(); // rimuove il primo
+            v.setEffettuata();
+
+            System.out.println("Visita effettuata: " + v);
+
+        } else {
+            System.out.println("Nessuna visita in coda.");
+        }
     }
 
     public void visualizzaPrenotazioni() {
-        System.out.println("\nPrenotazioni Dott. " + getCognome());
-        int quante = prenotazioni.size();
-        for (int i = 0; i < quante; i++) {
-            Visita v = prenotazioni.poll(); // Lo sfilo
-            System.out.println(v);           // Lo mostro
-            prenotazioni.add(v);            // Lo rimetto in fondo per non perderlo
+
+        System.out.println("\nPrenotazioni del Dottor " + getCognome() + " (" + specializzazione + ")");
+
+        for (int i = 0; i < prenotazioni.size(); i++) {
+
+            Visita v = prenotazioni.poll(); // tolgo
+            System.out.println(v);          // stampo
+            prenotazioni.add(v);           // reinserisco
         }
     }
 
     public boolean rimuoviVisita(String nomePaziente) {
+
         boolean rimossa = false;
-        int quante = prenotazioni.size();
-        for (int i = 0; i < quante; i++) {
+
+        for (int i = 0; i < prenotazioni.size; i++) {
+
             Visita v = prenotazioni.poll();
-            // Se trovo il paziente e non è ancora stata effettuata e non ne ho già rimossa una
-            if (v.getNomePaziente().equalsIgnoreCase(nomePaziente) && v.getStato().equals("prenotata") && !rimossa) {
-                rimossa = true; 
-                // NON lo riaggiungo: l'elemento è rimosso
+
+
+            if (v.getNomePaziente().equals(nomePaziente) && v.getStato().equals("prenotata") && !rimossa) {
+
+                rimossa = true; // viene eliminata
+
             } else {
-                prenotazioni.add(v);
+                prenotazioni.add(v); // la rimetto in coda
             }
         }
+
         return rimossa;
     }
-
-    public void effettuaProssimaVisita() {
-        if (!prenotazioni.isEmpty()) {
-            Visita v = prenotazioni.poll();
-            v.setEffettuata();
-            System.out.println("Visita completata per: " + v.getNomePaziente());
-        } else {
-            System.out.println("Coda vuota.");
-        }
-    }
-
-    public Queue<Visita> getPrenotazioni() { return prenotazioni; }
 }
