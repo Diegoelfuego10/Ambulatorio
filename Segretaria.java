969766f9a68d988443cc159cf9de4c97f5a559b2
Diegoerfuego <diegoraffaelli08@gmail.com>
2026-02-24 07:34:44 +0100

    Segretaria.java

--- Segretaria.java
+++ Segretaria.java
@@ -1,58 +1,34 @@
 import java.util.ArrayList;
 
 public class Segreteria {
-
     private ArrayList<Medico> medici;
 
     public Segreteria() {
         medici = new ArrayList<>();
     }
 
-    public void aggiungiMedico(Medico m) {
-        medici.add(m);
-    }
+    public void aggiungiMedico(Medico m) { medici.add(m); }
 
-    public void prenotaVisita(String nomePaziente, String data, String orario, Medico medico) {
-
-        Visita v = new Visita(nomePaziente, data, orario);
-        medico.aggiungiVisita(v);
-
-        System.out.println("Visita prenotata con il Dottor " + medico.getCognome());
+    public void prenotaVisita(String nomePaziente, String data, String orario, int indiceMedico) {
+        if (indiceMedico >= 0 && indiceMedico < medici.size()) {
+            Medico m = medici.get(indiceMedico);
+            Visita v = new Visita(nomePaziente, data, orario);
+            m.aggiungiVisita(v);
+            System.out.println("Prenotazione registrata con Dott. " + m.getCognome());
+        }
     }
 
     public void cercaPrenotazione(String nomePaziente) {
-
         for (int i = 0; i < medici.size(); i++) {
-
             Medico m = medici.get(i);
-
-            int size = m.getPrenotazioni().size();
-
-            for (int j = 0; j < size; j++) {
-
+            int nVisite = m.getPrenotazioni().size();
+            for (int j = 0; j < nVisite; j++) {
                 Visita v = m.getPrenotazioni().poll();
-
-                if (v.getNomePaziente().equals(nomePaziente)) {
-
-                    System.out.println("Trovata visita con il Dottor " + m.getCognome());
-                    System.out.println(v);
+                if (v.getNomePaziente().equalsIgnoreCase(nomePaziente)) {
+                    System.out.println("Trovata: " + v + " con Dott. " + m.getCognome());
                 }
-
-                m.getPrenotazioni().add(v); // reinserisco
+                m.getPrenotazioni().add(v);
             }
         }
-    }
-
-    public void cancellaVisita(String nomePaziente) {
-
-        for (int i = 0; i < medici.size(); i++) {
-
-            if (medici.get(i).rimuoviVisita(nomePaziente)) {
-                System.out.println("Visita cancellata.");
-                return;
-            }
-        }
-
-        System.out.println("Visita non trovata o già effettuata.");
     }
 }
