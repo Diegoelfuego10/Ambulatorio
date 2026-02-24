b8afdf8ea7d907c6c5ffa80f4c473c5d5de71095
Diegoerfuego <diegoraffaelli08@gmail.com>
2026-02-24 07:36:07 +0100

    Visita.java

--- Visita.java
+++ Visita.java
@@ -1,34 +1,25 @@
 public class Visita {
-    private string nomePaziente;
-    private string data;
-    private string orario;
-    private string stato;
+    private String nomePaziente;
+    private String data;
+    private String orario;
+    private String stato; // "prenotata" o "effettuata"
 
-    public Visita(String nomePaziente, String data, String orario){
-        this.nomePaziente=nomePaziente;
-        this.data=data;
-        this.orario=orario;
-        this.stato="prenotata";
+    public Visita(String nomePaziente, String data, String orario) {
+        this.nomePaziente = nomePaziente;
+        this.data = data;
+        this.orario = orario;
+        this.stato = "prenotata";
     }
 
-    public String getNomePaziente() {
-        return nomePaziente;
+    public void setEffettuata() {
+        this.stato = "effettuata";
     }
 
-    public string getData() {
-        return data;
-    }
-
-    public string getOrario() {
-        return orario;
-    }
-
-    public string getStato(){
-        return stato;
-    }
+    public String getNomePaziente() { return nomePaziente; }
+    public String getStato() { return stato; }
 
     @Override
     public String toString() {
-        return "Visita di: " + nomePaziente + "in data: " + data + "orario: " + orario + "stato visita: " +  stato;
+        return "[Paziente: " + nomePaziente + " | Data: " + data + " | Ora: " + orario + " | Stato: " + stato + "]";
     }
 }
