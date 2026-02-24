c605e6900824cb89440f0d29c93fdaeeb74aab21
Diegoerfuego <diegoraffaelli08@gmail.com>
2026-02-24 21:23:18 +0100

    Persona.java

--- Persona.java
+++ Persona.java
@@ -1,87 +1,18 @@
-import java.util.Date;
-import java.util.Scanner;
-
 public class Persona {
-    String nome;
-    String cognome;
-    String sesso;
-    String dataDiNascita;
+    protected String nome, cognome, sesso, dataDiNascita;
 
-
-    public void setNome(String nome){
-        this.nome=nome;
-    }
-
-    public void setCognome(String cognome) {
+    public Persona(String nome, String cognome, String sesso, String dataDiNascita) {
+        this.nome = nome;
         this.cognome = cognome;
-    }
-
-    public void setSesso(String sesso) {
         this.sesso = sesso;
-    }
-
-    public void setDataDiNascita(String dataDiNascita) {
         this.dataDiNascita = dataDiNascita;
     }
 
-    public String getNome
-            () {
-        return nome;
-    }
-
-    public String getCognome
-            () {
-        return cognome;
-    }
-
-    public String getSesso
-            () {
-        return sesso;
-    }
-
-    public String getDataDiNascita() {
-        return dataDiNascita;
-    }
+    public String getNome() { return nome; }
+    public String getCognome() { return cognome; }
 
     @Override
     public String toString() {
-        return "Persona" +
-                "nome='" + nome + '\'' +
-                ", cognome='" + cognome + '\'' +
-                ", sesso='" + sesso + '\'';
+        return nome + " " + cognome + " (" + sesso + ", nato/a il " + dataDiNascita + ")";
     }
-
-    public boolean equals (Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Persona)) {
-            return false;
-        }
-
-
-        Persona persona = (Persona) o;
-
-        return persona.getNome().equals(this.nome) &&
-                persona.getCognome().equals(this.cognome) &&
-                persona.getSesso().equals(this.sesso);
-    }
-
-    public void richiestaDati(Scanner scanner){
-
-
-        System.out.printf("Persona " );
-
-        System.out.printf("Inserisci nome");
-        setNome(scanner.next());
-
-
-        System.out.printf("Inserisci cognome");
-        setCognome(scanner.next());
-
-
-        System.out.printf("Inserisci sesso");
-        setSesso(scanner.next());
-
-
-    }
-
 }
