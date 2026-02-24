fe7bf34203e66996607a61bd6cd53710ae6130f9
Diegoerfuego <diegoraffaelli08@gmail.com>
2026-02-24 21:22:52 +0100

    Paziente.java

--- Paziente.java
+++ Paziente.java
@@ -1,3 +1,14 @@
-public class Paziente extends Persona{
+public class Paziente extends Persona {
+    private String codiceFiscale, telefono;
 
+    public Paziente(String nome, String cognome, String sesso, String dataNascita, String cf, String tel) {
+        super(nome, cognome, sesso, dataNascita);
+        this.codiceFiscale = cf;
+        this.telefono = tel;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (CF: " + codiceFiscale + " | Tel: " + telefono + ")";
+    }
 }
